0299ffd94efac1bbbd027002ee3fe3386a3da29b
mayurpatil-dev <patilmayur0204@gmail.com>
2025-09-18 09:31:03 +0530

    Add new changes of web blank issue

--- createxyz-project/apps/web/src/app/page.jsx
+++ createxyz-project/apps/web/src/app/page.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Building2, Users, ClipboardCheck } from 'lucide-react';
-import './global.css';
 
 
 export default function HomePage() {
